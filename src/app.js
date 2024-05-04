0d92ba213005750ef1e22b36b325081576b47307
cleisonsantos <css.cleison@outlook.com>
2024-05-03 21:44:56 -0300

    Criado a página inicial.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-    res.send('<h1>Criptoecho</h1>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
